dcbfc16b3db78dd1b97654b867a6b84485f4d1e9
gloriafercu <gloriafercu@gmail.com>
2018-10-19 17:44:47 +0200

    solved styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const seeMoreButtons = document.querySelector('.more-link');
+const articles = document.querySelectorAll('.article__container');
+console.log(seeMoreButtons);
+console.log(articles);
+
+function seeMoreArticles() {
+  for (let i = 0; i < articles.length; i++) {
+    articles[i].classList.remove('not_mobile');
+
+  }
+
+}
+seeMoreButtons.addEventListener('click', seeMoreArticles);
